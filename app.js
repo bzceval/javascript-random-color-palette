4aa61a8e9068b40934289df05e319c805fd45737
bzceval <busraaceval@gmail.com>
2022-08-30 18:14:30 +0300

    fixed random color variable

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
+console.log("js bağlandı");
 const containerEl = document.querySelector(".container");
-for (let index = 0; index < 30; index++) {
+for (let index = 0; index < 29; index++) {
   const colorContainerEl = document.createElement("div");
   colorContainerEl.classList.add("color-container");
   containerEl.appendChild(colorContainerEl);
 }
 
-const colorContainerEls = document.querySelectorAll(".color-container");
-generateColors();
-function generateColors() {
-  colorContainerEls.forEach((colorContainerEl) => {
-    const newColorCode = randomColor();
-    colorContainerEl.style.backgroundColor = "#" + newColorCode;
-    colorContainerEl.innerText = "#" + newColorCode;
-  });
-}
-
+// const colorContainerEls = document.querySelectorAll(".color-container");
+// generateColors();
+// function generateColors() {
+//   colorContainerEls.forEach((colorContainerEl) => {
+//     const newColorCode = randomColor();
+//     colorContainerEl.style.backgroundColor = "#" + newColorCode;
+//     colorContainerEl.innerText = "#" + newColorCode;
+//   });
+// }
+randomColor()
 function randomColor() {
   const chars = "0123456789abcdef";
   const colorCodeLength = 6;
-  let color = "";
+  let colorCode = "";
   for (let index = 0; index < colorCodeLength; index++) {
-    const randomNum = Math.floor(Math.random() * chars.length);
-    color += chars.substring(randomNum, randomNum + 1);
+    const randomNum = Math.floor(Math.random() * chars.length); 
+    colorCode += chars.substring(randomNum, randomNum + 1);
+    console.log(colorCode, randomNum)
   }
-  return colorCode;
+  // return colorCode;
 }
