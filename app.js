d81bf3e00fecd18205fe4bbd78a7486f05312c68
bzceval <busraaceval@gmail.com>
2022-08-30 18:00:47 +0300

    add style

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ for (let index = 0; index < 30; index++) {
   colorContainerEl.classList.add("color-container");
   containerEl.appendChild(colorContainerEl);
 }
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+generateColors();
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
+  });
+}
+
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let color = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    color += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
+}
